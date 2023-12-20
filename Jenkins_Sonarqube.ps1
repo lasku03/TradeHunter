595a5c08a3668d4d02d9a7197e5d1d5dfb08bb8f
Unai Laskurain Lauzirika <unai.laskurain@alumni.mondragon.edu>
2023-12-20 11:29:13 +0100

    Jenkins_Sonarqube.ps1 fixed

--- Jenkins_Sonarqube.ps1
+++ Jenkins_Sonarqube.ps1
@@ -1,4 +1,4 @@
-$sonarCommand = "mvn clean verify sonar:sonar -Dsonar.projectKey=TradeHunter -Dsonar.projectName='TradeHunter' -Dsonar.host.url=http://tradehunter.duckdns.org:9090 -Dsonar.token=sqp_446b24c9e7d429a2ab2df19ddd7e3438869ca294"
+$sonarCommand = "mvn clean verify sonar:sonar -f 03_Web/tradehunter.demo/pom.xml -Dsonar.projectKey=TradeHunter -Dsonar.projectName='TradeHunter' -Dsonar.host.url=http://tradehunter.duckdns.org:9090 -Dsonar.token=sqp_446b24c9e7d429a2ab2df19ddd7e3438869ca294"
 
 # Ejecuta el comando Maven
 Invoke-Expression -Command $sonarCommand
@@ -8,5 +8,5 @@ if ($LastExitCode -eq 0) {
     Write-Host "Sonarqube's static analysis of the project has been correct." -BackgroundColor Green
 } else {
     Write-Host "Error during Sonarqube's static analysis of the project. Exiting..." -BackgroundColor Red
-    Exit $LastExitCode
+    Exit
 }
