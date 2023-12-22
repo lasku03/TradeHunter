338e4d317d2be119ac47ecc9cee3d895d3ecd3f0
Unai Laskurain Lauzirika <unai.laskurain@alumni.mondragon.edu>
2023-12-22 14:49:22 +0100

    Remove duplicated Line

--- Jenkins_Publish.ps1
+++ Jenkins_Publish.ps1
@@ -21,8 +21,6 @@ if ($LASTEXITCODE -eq 0) {
     #Copy to the Web foled of TradeHunterAI
     $destinationFolder = "/home/trade_hunter_ai/web"
     Move-Item -Path $sourceJar -Destination $destinationFolder -Force
-
-    Move-Item -Path $sourceJar.FullName -Destination $destinationFolder -Force
 }
 else {
     # Si hubo un error en la construcción del JAR, mostrar mensaje de error
