ded3fa1ca692b2c5049fe22cdbf3bc50433bd245
Unai Laskurain Lauzirika <unai.laskurain@alumni.mondragon.edu>
2023-12-22 14:14:05 +0100

    Updated Jenkins_Publish.ps1 file to copy the .jar

--- Jenkins_Publish.ps1
+++ Jenkins_Publish.ps1
@@ -11,7 +11,19 @@ mvn package
 if ($LASTEXITCODE -eq 0) {
     # Si la construcción del JAR fue exitosa, mostrar mensaje
     Write-Host "Successfully built JAR file." -BackgroundColor Green
-} else {
+
+    # Mover el archivo JAR a la carpeta /home/trade_hunter_ai/web
+    # Obtener la versión del archivo POM
+    $versionPOM = (Select-String -Path pom.xml -Pattern '<version>(.*?)<\/version>').Matches.Groups[1].Value
+
+    # Buscar el archivo JAR con la versión específica en el directorio 'target'
+    $sourceJar = Get-ChildItem -Path "./target/" -Filter ("tradehunter.demo-$versionPOM.jar") -Recurse | Select-Object -First 1
+    
+    #Copy to the Web foled of TradeHunterAI
+    $destinationFolder = "/home/trade_hunter_ai/web"
+    Move-Item -Path $sourceJar -Destination $destinationFolder -Force
+}
+else {
     # Si hubo un error en la construcción del JAR, mostrar mensaje de error
     Write-Host "Error during the building of Trade Hunter JAR file." -BackgroundColor Red
     Exit $LASTEXITCODE
