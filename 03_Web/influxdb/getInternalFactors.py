7a51b81a5b1fe6a7ffb99e746c7b847554bf14ee
xabatm15 <xabat.maortua@alumni.mondragon.edu>
2024-01-05 15:55:15 +0100

    cambios no importantes

--- 03_Web/influxdb/getInternalFactors.py
+++ 03_Web/influxdb/getInternalFactors.py
@@ -1,4 +1,3 @@
-import requests
 from datetime import datetime
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -35,8 +34,8 @@ def reorganizar_resultados(result):
     return datos_reorganizados
 
 def get_internal_factors(start, stop):
-    url = "http://localhost:8086"
-    token = "cXCNQgiokEdLO_S9VLELWXYV4Vpgok-5t3k0Y3bS-CSC0PbK0myWpSyuJhh5Z-8PH3W7wSbjACzboLGeZMoCoQ=="
+    url = "http://tradehunter.duckdns.org:8086"
+    token = "KYrAp2dOqBHVBNr0XIT--Rm_PaSF2sWP_b7YZO-QD9MCPuejpe0Dzu7j3-6mxSK7xcCbVWJJYHdVgFdAQHbEFw=="
     org = "Trade Hunter"
     bucket = "Trade Hunter Real Time Data"
     measurement = "Internal_Factors"
