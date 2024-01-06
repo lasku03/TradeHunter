3af29a0ee7ee9aa14bdc8140757c5ac1bc28e274
xabatm15 <xabat.maortua@alumni.mondragon.edu>
2024-01-06 20:52:09 +0100

    Codigo para recuperar contraseña

--- 03_Web/tradehunter.demo/src/main/java/com/mondragon/tradehunter/demo/controllers/LoginController.java
+++ 03_Web/tradehunter.demo/src/main/java/com/mondragon/tradehunter/demo/controllers/LoginController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +35,21 @@ public class LoginController {
         }
         return responseEntity;
     }
+
+    @GetMapping(value = "/password/{email}", produces = { "application/json",
+            "application/xml" })
+    public ResponseEntity<User> getUser(@PathVariable String email) {
+        ResponseEntity<User> responseEntity;
+
+        User user = userService.getUserByEmail(email);
+
+        if(user != null){
+            responseEntity = new ResponseEntity<>(user, HttpStatus.OK);
+        }else{
+            responseEntity = ResponseEntity.notFound().build();
+        }
+        return responseEntity;
+
+    }
+
 }
