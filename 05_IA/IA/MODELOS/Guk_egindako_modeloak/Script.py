8f1fcafae902bb9433451f58b5d15ae09b289e52
Andoni <andoni.usandizaga@alumni.mondragon.edu>
2024-01-19 09:13:40 +0100

    Modelo de Prophet prediciion a la web

---
+++ 05_IA/IA/MODELOS/Guk_egindako_modeloak/Script.py
@@ -0,0 +1,33 @@
+import pickle
+import pandas as pd
+from prophet import Prophet
+import json
+
+  
+data = pd.read_csv("archive_2/merged_dataset1.csv")
+
+
+# Charge the model. Pkl archive.
+with open('modelo_prophet.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+    
+    data['ds'] = data['Date']
+    data['y'] = data['AdjClose']
+
+
+# Do the prediction using the created model
+def make_prediction(input_data):
+    forecast = model.predict(input_data)
+    return forecast
+
+
+predictions = make_prediction(data)
+ultimos_dos_yhat = predictions['yhat'].tail(2)
+
+result_json = ultimos_dos_yhat.to_json(orient="split")
+# Convierte el resultado a un diccionario JSON válido
+import json
+result_dict = json.loads(result_json)
+result_json = json.dumps({"yhat": result_dict["data"]}, indent=2)
+
