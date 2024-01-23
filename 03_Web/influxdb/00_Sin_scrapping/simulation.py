880e005c20433f49e74d04436c4bf349d0848d06
Unai Laskurain Lauzirika <unai.laskurain@alumni.mondragon.edu>
2024-01-23 14:33:08 +0100

    Serv.py fixed

--- 03_Web/influxdb/00_Sin_scrapping/simulation.py
+++ 03_Web/influxdb/00_Sin_scrapping/simulation.py
@@ -5,9 +5,13 @@ class Simulation:
         data = pd.read_csv("simulation/merged_dataset1.csv")
         last_row = data.iloc[-1]
 
-        for value in values:
-            last_row[value.name] = value.value
+        for entry in values:
+            name = entry['name']
+            value = entry['value']
+            
+            if name in last_row.index:
+                last_row[name] = value
 
         data.iloc[-1] = last_row
 
-        data.to_csv("simulation/mergedataset.csv", index=True)
+        data.to_csv("simulation/merged_dataset1.csv", index=True)
--- 03_Web/influxdb/simulation.py
+++ 03_Web/influxdb/simulation.py
@@ -5,24 +5,13 @@ class Simulation:
         data = pd.read_csv("simulation/merged_dataset1.csv")
         last_row = data.iloc[-1]
 
-        for value in values:
-            last_row[value.name] = value.value
+        for entry in values:
+            name = entry['name']
+            value = entry['value']
+            
+            if name in last_row.index:
+                last_row[name] = value
 
         data.iloc[-1] = last_row
 
-        data.to_csv("simulation/mergedataset.csv", index=True)
-
-class DataObject:
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-# Crear una lista de objetos
-data_objects = [
-    DataObject(name='Births', value=100),
-    DataObject(name='Defunciones', value=2.789),
-    DataObject(name='Total_debt', value=1.1234)
-]
-
-simulation_instance = Simulation()
-simulation_instance.update_excel(data_objects)
+        data.to_csv("simulation/merged_dataset1.csv", index=True)
