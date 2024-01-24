8e17a0748f8f6133dcd869a5553c98ca6bf20e79
Unai Laskurain Lauzirika <unai.laskurain@alumni.mondragon.edu>
2024-01-24 11:35:14 +0100

    Jenkins_Sonarqube_AI.sh

--- Jenkins_Sonarqube_AI.sh
+++ Jenkins_Sonarqube_AI.sh
@@ -7,6 +7,8 @@ sonarToken="sqp_6cd9a8d70893a822b4f20768fc900ad55d1c1de3"
 
 cd 03_Web/influxdb
 sudo coverage run -m unittest discover
+sudo coverage html
+sudo coverage xml
 
 sonarCommand="/home/unai_laskurain/sonar-scanner-5.0.1.3006-linux/bin/sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=. -Dsonar.host.url=${sonarHostUrl} -Dsonar.token=${sonarToken}"
 
